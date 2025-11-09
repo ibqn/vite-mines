f07b22a53870d2f55625b9c0f71f68761fd71951
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-09 17:57:41 +0100

    Refactor Cell component to improve tile state management and enhance click handling logic

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,7 +1,6 @@
-import { gameStateAtom, generateMinePositionsAtom, tilesAtom } from '@/store'
+import { gameStateAtom, generateMinePositionsAtom, revealTileAtom, tilesAtom } from '@/store'
 import { GameState, State, type Tile } from '@/types'
 import { useAtom, useSetAtom } from 'jotai'
-import { useImmerAtom } from 'jotai-immer'
 import { useEffect, useRef, type MouseEventHandler } from 'react'
 
 type CellProps = {
@@ -11,11 +10,13 @@ type CellProps = {
 export const Cell = ({ tile }: CellProps) => {
   const ref = useRef<HTMLDivElement>(null)
 
-  const [_, setTiles] = useImmerAtom(tilesAtom)
+  const [tiles, setTiles] = useAtom(tilesAtom)
   const generateMinePositions = useSetAtom(generateMinePositionsAtom)
+  const revealTile = useSetAtom(revealTileAtom)
   const [gameState, setGameState] = useAtom(gameStateAtom)
 
   const handleClick = (index: number) => () => {
+    console.log('Cell clicked:', index)
     if (gameState === GameState.idle) {
       generateMinePositions(index)
       setGameState(GameState.going)
@@ -25,10 +26,7 @@ export const Cell = ({ tile }: CellProps) => {
       return
     }
 
-    setTiles((tiles) => {
-      tiles[index].state = State.mine
-      return tiles
-    })
+    revealTile(index)
   }
 
   const handleRightClick =
@@ -36,24 +34,25 @@ export const Cell = ({ tile }: CellProps) => {
     (event) => {
       event.preventDefault()
 
-      setTiles((tiles) => {
-        tiles[index].state = State.marked
-        return tiles
-      })
+      setTiles(
+        tiles.map((tile) =>
+          tile.index === index ? { ...tile, state: tile.state === State.marked ? State.hidden : State.marked } : tile
+        )
+      )
     }
 
   useEffect(() => {
-    if (tile && ref.current) {
-      ref.current.setAttribute('data-state', tile.state)
+    if (tile.adjacentMinesCount && ref.current) {
+      ref.current.style.backgroundImage = `url(./images/type${tile.adjacentMinesCount}.svg)`
     }
-  }, [tile, ref])
+  }, [tile.adjacentMinesCount, ref])
 
   return (
     <div
       ref={ref}
-      className="flex size-16 cursor-pointer border border-gray-400 bg-white bg-center bg-no-repeat
+      className="flex size-16 cursor-pointer border border-gray-400 bg-gray-400 bg-center bg-no-repeat
         data-[state=marked]:bg-yellow-400 data-[state=marked]:bg-[url('./images/flag.svg')] data-[state=mine]:bg-red-700
-        data-[state=mine]:bg-[url('./images/mine.svg')]"
+        data-[state=mine]:bg-[url('./images/mine.svg')] data-[state=number]:bg-white"
       onClick={handleClick(tile.index)}
       onContextMenu={handleRightClick(tile.index)}
       data-state={tile.state}
