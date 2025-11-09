d7da2dad921e5a7538c0e3abf9b77ff7a05d131a
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-09 09:03:13 +0100

    Add mines count display and refactor App component layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,17 @@
 import { Board } from '@/components/board'
+import { useAtomValue } from 'jotai'
+import { mineCountAtom } from './store'
 
 export const App = () => {
+  const minesCount = useAtomValue(mineCountAtom)
+
   return (
     <main className="flex min-h-svh flex-col items-center gap-10 pt-20">
-      <h1 className="text-3xl font-bold uppercase">free mines</h1>
+      <div>
+        <h1 className="text-3xl font-bold uppercase">free mines</h1>
+        <p>Mines left: {minesCount}</p>
+      </div>
+
       <Board />
     </main>
   )
