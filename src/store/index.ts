d743dd8c3c57f908ae7b7fe44f1677e6598d743b
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-09 17:57:38 +0100

    Implement nearby tiles calculation and enhance tile revealing logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -41,3 +41,78 @@ export const generateMinePositionsAtom = atom(null, (get, set, exceptIndex: numb
 })
 
 export const gameStateAtom = atom<GameState>(GameState.idle)
+
+export const nearbyTilesAtom = atom((get) => (index: number) => {
+  const tiles = get(tilesAtom)
+  const boardSize = get(boardSizeAtom)
+
+  const nearbyTiles: Tile[] = []
+
+  for (let offsetX = -1; offsetX <= 1; offsetX++) {
+    for (let offsetY = -1; offsetY <= 1; offsetY++) {
+      if (offsetX === 0 && offsetY == 0) {
+        continue
+      }
+
+      const x = index % boardSize.width
+      const y = Math.floor(index / boardSize.width)
+
+      if (x + offsetX < 0 || x + offsetX >= boardSize.width || y + offsetY < 0 || y + offsetY >= boardSize.height) {
+        continue
+      }
+
+      const idx = index + offsetX + offsetY * boardSize.width
+      const tile = tiles[idx]
+
+      if (tile) {
+        nearbyTiles.push(tile)
+      }
+    }
+  }
+
+  return nearbyTiles
+})
+
+export const revealTileAtom = atom(null, (get, set, currentIndex: number) => {
+  console.log('Revealing tile:', currentIndex)
+  const tiles = get(tilesAtom)
+  const getNearbyTiles = get(nearbyTilesAtom)
+
+  const newTiles = tiles.map((tile) => tile)
+
+  const queue: number[] = [currentIndex]
+
+  while (queue.length > 0) {
+    const queueIndex = queue.shift()!
+    const tile = newTiles[queueIndex]
+
+    if (tile.state !== State.hidden) {
+      continue
+    }
+
+    if (tile.mine) {
+      newTiles[queueIndex].state = State.mine
+      set(tilesAtom, newTiles)
+      set(gameStateAtom, GameState.lost)
+      return
+    }
+    console.log('Revealing safe tile:', queueIndex, newTiles[queueIndex])
+
+    newTiles[queueIndex].state = State.number
+
+    const nearbyTiles = getNearbyTiles(queueIndex)
+    console.log('Nearby tiles:', nearbyTiles)
+
+    const nearbyMinesCount = nearbyTiles.filter((tile) => tile.mine).length
+    console.log('Nearby mines count:', nearbyMinesCount)
+
+    if (nearbyMinesCount === 0) {
+      for (const nearbyTile of nearbyTiles) {
+        queue.push(nearbyTile.index)
+      }
+    } else {
+      newTiles[queueIndex].adjacentMinesCount = nearbyMinesCount
+    }
+  }
+  set(tilesAtom, newTiles)
+})
